5d47fcd1585ae724759b6d6d751d66253277cb9b
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-06-15 20:58:10 -0400

    Doc

--- main.py
+++ main.py
@@ -9,9 +9,67 @@ from sklearn.linear_model import LogisticRegressionCV
 from sklearn import preprocessing as prep
 
 class Clinical_model:
+    """
+    class Clinical_model:  Cette classe sert à analyser les données du projet de Mathieu Godbout sur les outcomes
+    postopératoires de la chirurgie du trou maculaire.  Elle se charge de lire les données, les prétraiter, séparer aléatoirement
+    un ensemble de données d'entraînement et de test, initialiser
+    un modèle de régression logistique, entraîner le modèle avec les données d'entraînement et tester et valider le
+    modèle avec les données de test.  On peut ensuite rediviser les données aléatoirement afin de recommencer les
+    expériences d'entraînement et de test, dans le but de vérifier la fiabilité et reproducibilité du modèle.
+
+    Attributs:
+    ---------
+    mode (string): Indique à l'instance si elle est prête à prétraiter les donner, entrainer le modèle ou tester le
+    modèle.  Par-exemple, le modèle ne peut être testé avant d'avoir été entraîné...
+
+    validation (dict): Dictionnaire contenant les résultats des tests de validation, pour une exécution donnée d'une
+    séquence d'entraînement et test.
+
+    stats (dict list): Liste des dictionnaires de validation pour toutes les exécutions consécutives des séquences d'entrainement
+    et tests. Constitue l'output principal de la classe.
+
+    df_total (DataFrame): Objet contenant l'entièreté des données cliniques traitées.  C'est à partir de cet objet qu'on
+    divise aléatoirement un ensemble entrainement et un ensemble test.
+
+    log_reg_model (LogisticRegressionCV): Modèle de régression logistique, fourni par sklearn
+
+    df_train (DataFrame): Objet contenant les données d'entraînement pour une exécution donnée.
+
+    df_test (DataFrame): Objet contenant les données de test pour une exécution donnée.
+
+    y_train (Series): Objet contenant la variable dépendante de l'ensemble d'entraînement
+
+    X_train (DataFrame): Objet contenannt les variables indépendantes de l'ensemble d'entraînement
+
+    y_test (Series): Objet contenant la variable dépendante de l'ensemble de test
+
+    X_test (DataFrame): Objet contenant les variables indépendantes de l'ensemble de test
+
+    Méthodes
+    --------
+
+    __init__ (chemin): lit les fichiers de données dans le nom de chemin complet chemin/clinical_data_train.csv, et
+    initialise les attributs afin de préparer la première séquence d'entraînement.
+
+    initialiser_les_donnees(): Sépare les données en ensembles de test et d'entraînement.  Appelle la méthode de
+    classe pre_traiter_les_donnees afin de terminer le prétraitement.
+
+    entrainer_le_modele(): Permet le calcul des coefficients du modèle.
+
+    tester_le_modele(): Applique le modèle aux données test.  Avec les résultats, calcule le score d'exactitude,
+    l'aire sous la courbe de la courbe récepteur-opérateur, la matrice de confusion, et d'autres statistiques du modèle.
+    Stocke les résultats dans une liste de dictionnaires.
+
+    reinitialiser_le_modele(): Réalloue les ensembles d'entraînement et de test à partir des données totales.  Réinitialise
+    les paramètres afin de pouvoir réentrainer et retester le modèle.
+
+    pre_traiter_les_donnees(): Élimine les lignes comportant des données manquantes, élimine les colonnes non désirées,
+    calcule la colonne correspondant à l'outcome primaire de l'étude.
+
+    """
 
     @classmethod
-    def pre_traitement_donnees (cls, df):
+    def pre_traiter_les_donnees (cls, df):
         """
         Prétraitement d'un dataframe pour entraînement ou test.
         :param df: Dataframe provenant des données de https://www.kaggle.com/datasets/mathieugodbout/oct-postsurgery-visual-improvement lu
@@ -70,14 +128,14 @@ class Clinical_model:
         self.df_total = pd.concat([self.df_total, pd.read_csv(chemin_donnees + "clinical_data_val.csv", na_values='-9')], ignore_index=True)
         self.df_total = pd.concat([self.df_total, pd.read_csv(chemin_donnees + "clinical_data_test.csv", na_values='-9')], ignore_index=True)
 
-        self.initialiser_donnees()
+        self.initialiser_les_donnees()
 
         # Configuration du modèle de régression logistique: log_reg_model
         self.log_reg_model = LogisticRegressionCV(cv=10)
 
         self.mode = "entrainement"
 
-    def entrainer(self):
+    def entrainer_le_modele(self):
         """
         Si les données ont été prétraitées, on entraîne le modèle.
         """
@@ -88,7 +146,7 @@ class Clinical_model:
         else:
             raise RuntimeError("Tentative d'entrainer un modèle dont les données ne sont pas traitées.")
 
-    def tester(self):
+    def tester_le_modele(self):
         """
         Une fois le modèle entraîné, on utilise les données de test pour extraire les scores de validation standard du
         modèle.
@@ -113,7 +171,7 @@ class Clinical_model:
         else:
             raise RuntimeError("Le modèle n'a pas été entrainé.")
 
-    def initialiser_donnees(self):
+    def initialiser_les_donnees(self):
         """
         Séparation des données en ensembles de test et d'entrainement.  Prétraitement des données.
         """
@@ -124,21 +182,21 @@ class Clinical_model:
             self.df_test = self.df_train.sample(frac=0.2).reset_index(drop=True)
 
             # On va formatter les séries pour l'entraînement et les tests
-            self.y_train, self.X_train = Clinical_model.pre_traitement_donnees(self.df_train)
-            self.y_test, self.X_test = Clinical_model.pre_traitement_donnees(self.df_test)
+            self.y_train, self.X_train = Clinical_model.pre_traiter_les_donnees(self.df_train)
+            self.y_test, self.X_test = Clinical_model.pre_traiter_les_donnees(self.df_test)
 
 
         else:
             raise RuntimeError("Tentative de réinitialisation des données")
 
-    def reinitialiser (self):
+    def reinitialiser_le_modele (self):
         """
         Après une exécution d'entrainement et test, repréparer les données et le modèle afin de réentrainer et retester.
         """
 
         if self.mode == "valide":
            self.mode = "pretraitement"
-           self.initialiser_donnees()
+           self.initialiser_les_donnees()
            self.log_reg_model = LogisticRegressionCV(cv=10)
            self.validation = {}
            self.mode = "entrainement"
@@ -155,7 +213,7 @@ class Clinical_model:
             liste_aurocs.append(r['auroc'])
         return pd.Series(liste_aurocs)
 
-    def lire_coefs(self):
+    def lire_coefficients(self):
         """
         Retourne la liste des coefficients de régression calculés à chaque exécution
         :return: pd.Dataframe liste des listes des coefficients pour chaque exécution
@@ -172,15 +230,15 @@ if __name__ == "__main__":
 
     for i in range(20):
         print(f"Run # {i}", )
-        mod.entrainer()
-        mod.tester()
-        mod.reinitialiser()
+        mod.entrainer_le_modele()
+        mod.tester_le_modele()
+        mod.reinitialiser_le_modele()
 
     df_auroc = mod.lire_aurocs()
     print(df_auroc.to_string())
     print(df_auroc.describe().to_string())
 
-    df_coefs = mod.lire_coefs()
+    df_coefs = mod.lire_coefficients()
     print(df_coefs)
     #print(df_auroc.describe().to_string())
 
