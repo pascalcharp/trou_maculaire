c7d09a99c1a8dec4508c7639343298e63066ca82
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-07-18 13:02:20 -0400

    auroc

--- DL_model.py
+++ DL_model.py
@@ -157,7 +157,7 @@ class DLM_module(pl.LightningModule):
         ts_loss = self.loss(y_hat, y)
         ts_accuracy = self.accuracy(y_hat, y)
         ts_auroc = self.auroc(y_hat, y)
-        self.log("train_loss", ts_loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_loss", ts_loss, on_epoch=True, logger=True)
         self.log("train_accuracy", ts_accuracy, on_epoch=True, logger=True)
         self.log("train_auroc", ts_auroc, on_epoch=True, logger=True)
         return ts_loss
@@ -173,7 +173,7 @@ class DLM_module(pl.LightningModule):
         vs_loss = self.loss(y_hat, y)
         vs_accuracy = self.accuracy(y_hat, y)
         vs_auroc = self.auroc(y_hat, y)
-        self.log("validation_loss", vs_loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("validation_loss", vs_loss, on_epoch=True, logger=True)
         self.log("validation_accuracy", vs_accuracy, on_epoch=True, logger=True)
         self.log("validation_auroc", vs_auroc, on_epoch=True, logger=True)
         return vs_loss
@@ -186,7 +186,7 @@ class DLM_module(pl.LightningModule):
         ts_auroc = self.auroc(y_hat, y)
         self.log("test_loss", ts_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
         self.log("test_accuracy", ts_accuracy, on_epoch=True, logger=True)
-        self.log("test_auroc", on_epoch=True, logger=True)
+        self.log("test_auroc", ts_auroc, on_epoch=True, logger=True)
         return ts_loss
 
 
