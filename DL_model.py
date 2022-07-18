7f5ecc1a5c58fd65c5e5662aff1a7e76392e986a
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-07-18 12:44:09 -0400

    auroc

--- DL_model.py
+++ DL_model.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 import pytorch_lightning as pl
 import pandas as pd
 import numpy as np
+import torchmetrics
 from PIL import Image
 import torchvision.transforms as transforms
 import json
@@ -147,12 +148,18 @@ class DLM_module(pl.LightningModule):
         super(DLM_module, self).__init__()
         self.model=model()
         self.loss = nn.MSELoss()
+        self.accuracy = torchmetrics.Accuracy()
+        self.auroc = torchmetrics.AUROC()
 
     def training_step(self, batch, batch_idx):
         X, y = batch
-        y_hat = self.model.forward(X)
-        ts_loss = self.loss(torch.squeeze(y_hat), y)
-        self.log("train_loss", ts_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        y_hat = torch.squeeze(self.model.forward(X))
+        ts_loss = self.loss(y_hat, y)
+        ts_accuracy = self.accuracy(y_hat, y)
+        ts_auroc = self.auroc(y_hat, y)
+        self.log("train_loss", ts_loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("train_accuracy", ts_accuracy, on_epoch=True, logger=True)
+        self.log("train_auroc", ts_auroc, on_epoch=True, logger=True)
         return ts_loss
 
 
@@ -162,15 +169,25 @@ class DLM_module(pl.LightningModule):
 
     def validation_step(self, batch, batch_idx):
         X, y = batch
-        y_hat = self.model.forward(X)
-        vs_loss = self.loss(torch.squeeze(y_hat), y)
-        self.log("validation_loss", vs_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        y_hat = torch.squeeze(self.model.forward(X))
+        vs_loss = self.loss(y_hat, y)
+        vs_accuracy = self.accuracy(y_hat, y)
+        vs_auroc = self.auroc(y_hat, y)
+        self.log("validation_loss", vs_loss, on_epoch=True, prog_bar=True, logger=True)
+        self.log("validation_accuracy", vs_accuracy, on_epoch=True, logger=True)
+        self.log("validation_auroc", vs_auroc, on_epoch=True, logger=True)
+        return vs_loss
 
     def test_step(self, batch, batch_idx):
         X, y = batch
-        y_hat = self.model.forward(X)
-        ts_loss = self.loss(torch.squeeze(y_hat), y)
+        y_hat = torch.squeeze(self.model.forward(X))
+        ts_loss = self.loss(y_hat, y)
+        ts_accuracy = self.accuracy(y_hat, y)
+        ts_auroc = self.auroc(y_hat, y)
         self.log("test_loss", ts_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        self.log("test_accuracy", ts_accuracy, on_epoch=True, logger=True)
+        self.log("test_auroc", on_epoch=True, logger=True)
+        return ts_loss
 
 
 
