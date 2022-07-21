dfd58f6beff2c43e10a921c62a0f830ebf9ec65a
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-07-21 14:45:27 -0400

    num_workers=6

--- DL_model.py
+++ DL_model.py
@@ -223,9 +223,9 @@ class DLM_trainer:
         self.validation_dataset = DLM_dataset(directory, set = "val")
         self.test_dataset = DLM_dataset(directory, set="test")
 
-        self.train_loader = DataLoader(self.training_dataset, batch_size=32, num_workers=0, shuffle=True)
-        self.validation_loader = DataLoader(self.validation_dataset, batch_size=42, num_workers=0, shuffle=True)
-        self.test_loader = DataLoader(self.test_dataset, batch_size=34, num_workers=0, shuffle=True)
+        self.train_loader = DataLoader(self.training_dataset, batch_size=32, num_workers=6, shuffle=True)
+        self.validation_loader = DataLoader(self.validation_dataset, batch_size=42, num_workers=6, shuffle=True)
+        self.test_loader = DataLoader(self.test_dataset, batch_size=34, num_workers=6, shuffle=True)
 
     def train(self, epochs=1000):
         for epoch in range(epochs):
