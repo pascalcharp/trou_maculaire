956860eed23c7434b74c122a52507384eb96432a
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-07-22 11:31:00 -0400

    validation V et H divisées en 2 pour avoir bon modèle d'inférence

--- DL_model.py
+++ DL_model.py
@@ -231,11 +231,13 @@ class DLM_trainer:
         self.optimizer = torch.optim.Adam(self.model.parameters(), lr=1.0e-4)
 
         self.training_dataset = DLM_dataset(directory, set="train", direction="both")
-        self.validation_dataset = DLM_dataset(directory, set = "val", direction="both")
+        self.validation_H_dataset = DLM_dataset(directory, set = "val", direction="H")
+        self.validation_V_dataset = DLM_dataset(directory, set="val", direction="V")
         self.test_dataset = DLM_dataset(directory, set="test", direction="both")
 
         self.train_loader = DataLoader(self.training_dataset, batch_size=32, num_workers=6, shuffle=True)
-        self.validation_loader = DataLoader(self.validation_dataset, batch_size=42, num_workers=6, shuffle=True)
+        self.validation_H_loader = DataLoader(self.validation_H_dataset, batch_size=21, num_workers=6, shuffle=True)
+        self.validation_V_loader = DataLoader(self.validation_V_dataset, batch_size=21, num_workers=6, shuffle=True)
         self.test_loader = DataLoader(self.test_dataset, batch_size=34, num_workers=6, shuffle=True)
 
     def train(self, epochs=500):
@@ -276,31 +278,29 @@ class DLM_trainer:
                 self.model.eval()
                 with torch.no_grad():
 
-                    for X, y in self.validation_loader:
-                        if torch.cuda.is_available():
-                            X, y = X.cuda(), y.cuda()
-                        logits = self.model(X)
-                        pred_probab = torch.sigmoid(logits)
-                        loss = self.loss(pred_probab, y)
+                    V_loss, V_labels, V_probabilities = self.perform_inference_on(self.validation_V_loader)
+                    H_loss, H_labels, H_probabilities = self.perform_inference_on(self.validation_H_loader)
 
-                        labels = y.cpu().numpy()
-                        probabilities = pred_probab.cpu().numpy()
-                        # fpr, tpr, thr = metrics.roc_curve(y_true=labels, y_score=probabilities, pos_label=1.0)
-                        auroc = sklearn.metrics.roc_auc_score(labels, probabilities)
-                        # F1 = sklearn.metrics.f1_score(labels, probabilities)
-                        # accuracy = sklearn.metrics.accuracy_score(labels, probabilities)
+                assert(V_labels == H_labels)
+                probabilities = 0.5 * (V_probabilities + H_probabilities)
 
 
 
-                        validation_auroc += auroc
-                        # validation_F1 += F1
-                        validation_loss += loss.item()
-                        # validation_accuracy += accuracy
+                auroc = sklearn.metrics.roc_auc_score(V_labels, probabilities)
+                # F1 = sklearn.metrics.f1_score(labels, probabilities)
+                # accuracy = sklearn.metrics.accuracy_score(labels, probabilities)
+                # fpr, tpr, thr = metrics.roc_curve(y_true=labels, y_score=probabilities, pos_label=1.0)
+
+
+                validation_auroc += auroc
+                # validation_F1 += F1
+                validation_loss += 0.5 * (V_loss + H_loss)
+                # validation_accuracy += accuracy
 
 
 
-                validation_loss = validation_loss / len(self.validation_loader)
-                validation_auroc = validation_auroc / len(self.validation_loader)
+                validation_loss = validation_loss / len(self.validation_V_loader)
+                validation_auroc = validation_auroc / len(self.validation_V_loader)
                 if (validation_auroc > max_auroc):
                     max_auroc = validation_auroc
                     best_model = self.model.state_dict()
@@ -317,10 +317,20 @@ class DLM_trainer:
         print("Sauvegarde du modèle")
         torch.save(best_model, self.save_model_path)
 
+    def perform_inference_on(self, validation_loader):
 
+        for X, y in validation_loader:
 
+            if torch.cuda.is_available():
+                X, y = X.cuda(), y.cuda()
+            logits = self.model(X)
+            pred_probab = torch.sigmoid(logits)
+            loss = self.loss(pred_probab, y)
 
+            labels = y.cpu().numpy()
+            probabilities = pred_probab.cpu().numpy()
 
+            return loss.item(), labels, probabilities
 
 
 # def main(data_directory, train_dataset_batch_size, enable_progress_bar):
