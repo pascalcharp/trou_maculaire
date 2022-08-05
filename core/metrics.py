22463aafbdcce77bdd8d0ab383d2f09351269b32
Pascal Charpentier <charpentier_pasc@hotmail.com>
2022-08-05 16:11:05 -0400

    doc dans metrics

--- core/metrics.py
+++ core/metrics.py
@@ -2,8 +2,14 @@ import sklearn.metrics
 
 
 def get_metrics_from_prediction(y, pred_proba):
-    metrics = {}
+    """
+    Calcule les données de validation d'après les probabilités retournées par un modèle.
+    :param y: Un tableau à une dimension contenant des labels
+    :param pred_proba: Un tableau à une dimension contenant les probabilités retournées par un modèle
+    :return: un dict contenant: auroc, score-f1, accuracy, sensibilité, spécificité
+    """
 
+    metrics = {}
     metrics["auroc"] = sklearn.metrics.roc_auc_score(y, pred_proba)
     tn, fp, fn, tp = sklearn.metrics.confusion_matrix(y, pred_proba > 0.5).ravel()
     metrics["f1"] = 2 * tp / (2 * tp + fp + fn)
